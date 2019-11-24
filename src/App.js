159a7f8d5233dfbce58c8bdb9bdd8cfe7020e9ea
tony3fk <tony3fk@yahoo.es>
2019-11-24 20:25:16 +0100

    ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Buscador from "./componentes/Buscador";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  datosBusqueda = termino => {
+    console.log(termino);
+  };
+
+  render() {
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de imágenes</p>
+          <Buscador datosBusqueda={this.datosBusqueda} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
