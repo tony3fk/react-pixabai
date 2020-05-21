f5e1d016f588a7c54baf4e9e38d0ec5355658039
tony3fk <tony3fk@yahoo.es>
2020-05-21 19:27:16 +0200

    Updates

---
+++ precache-manifest.8b076d788d49d8d0db37959be99ad051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51570791177488fb5a9f516209e4af9c",
+    "url": "/react-pixabai/index.html"
+  },
+  {
+    "revision": "9b07e90cc3490e95c808",
+    "url": "/react-pixabai/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e53e9539e29cd315697e",
+    "url": "/react-pixabai/static/js/2.409b56fb.chunk.js"
+  },
+  {
+    "revision": "9b07e90cc3490e95c808",
+    "url": "/react-pixabai/static/js/main.9d20f8f1.chunk.js"
+  },
+  {
+    "revision": "5115c9a9c1942c86e98d",
+    "url": "/react-pixabai/static/js/runtime-main.09206e21.js"
+  }
+]);
