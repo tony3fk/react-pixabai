d57b7410ea086b7823dfe844a7a7e8bd3171a90b
tony3fk <tony3fk@yahoo.es>
2020-05-21 19:38:25 +0200

    publish

--- src/componentes/Imagen.js
+++ src/componentes/Imagen.js
@@ -8,7 +8,7 @@ const Imagen = (props) => {
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
-                <a href={largeImageURL} target="_blank"><img src={previewURL} alt={tags} className="card-img-top image-fluid"/></a>
+                <a href={largeImageURL} rel="noopener noreferrer"><img src={previewURL} alt={tags} className="card-img-top image-fluid"/></a>
             </div>
             <div className="card-body">
                 <p className="card-text">{likes} Me gusta</p>
